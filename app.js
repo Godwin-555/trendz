2815b9a31c878f49e62d1eb62a2bb4c4651bd6ee
Godwin-555 <chibuikejamb2019@gmail.com>
2020-10-08 15:38:42 -0700

    godwin

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-  
+let LoginForm = document.querySelector(".Login");
+
+let LoginBtn = document.querySelector(".Login-btn");
+
+let Categorybtn = document.querySelector(".category-btn")
+
+let Categorycontent = document.querySelector(".category-content")
+
+LoginBtn.addEventListener("click",function () {
+    LoginForm.classList.toggle("open")
+    
+})
+
+Categorybtn.addEventListener("click",function () {
+    Categorycontent.classList.toggle("open")
+    
+})
